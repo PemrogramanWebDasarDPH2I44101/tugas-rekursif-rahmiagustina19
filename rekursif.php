68524160086008115d7204b644af1e977668bf6e
rahmiagustina19 <42725490+rahmiagustina19@users.noreply.github.com>
2018-09-10 19:55:33 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,37 @@
 <?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+function recursive($kolom, $z){
+	global $i;
+	recursive2($i, $kolom, $z);
+	echo "<br>";
+	if (--$kolom > $i) {
+		recursive($kolom, $z);
+	}
 }
-rekursif(5);
+function recursive2($y, $kolom, $print){
+	global $i;
+	if ($y > $i - $kolom) {
+		echo "$print";
+		recursive2($y - 1, $kolom, $print);
+	}
+}
+$i = 0;
+recursive(5, "*");
+echo"<br>/*====================================================================================*/<br><br>";
+$angka=5;
+function recursive3($a,$b,$angka){
+	if ($a<=$angka) {
+		if ($b<=$a) {
+			echo $b;
+			$b++;
+			recursive3($a,$b,$angka);
+		}
+		else{
+			echo "<br>";
+			$a++;
+			recursive3($a,1,$angka);
+		}
+	}
+}
+	recursive3(1,1,$angka);
+
 ?>
